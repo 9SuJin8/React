8c735132b9fde5d5ffa497d596350e136dbcf9e2
SuJin Choi <68210266+reader-wh94@users.noreply.github.com>
2021-04-10 22:29:39 +0900

    Delete personal API key

--- JavaScript/weather.js
+++ JavaScript/weather.js
@@ -1,6 +1,6 @@
 const weather = document.querySelector(".js-weather");
 
-const API_KEY = "e036ea478adf9a38b44ddb0de7b0b574";
+const API_KEY = "yourAPIkey";
 const COORDS = 'coords';
 
 function getWeather(lat, lng) {
